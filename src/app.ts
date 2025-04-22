a44b19d2c61cf86b8cf2e8483487b6f8afe1ce1e
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2025-04-22 09:50:14 +0530

    v1 router mounting function added
    Added schema file

---
+++ src/app.ts
@@ -0,0 +1,11 @@
+import express from "express"
+import setup_v1_router from "./v1/app-router"
+
+const app: express.Express = express()
+const PORT = 5729
+
+setup_v1_router(app)
+
+app.listen(PORT, "0.0.0.0", () => {
+    console.log(`app listening to port ${PORT}`)
+})
