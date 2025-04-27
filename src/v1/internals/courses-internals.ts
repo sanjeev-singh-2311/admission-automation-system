05091fa67c3ae38b1efc8d9039e0ab7abd703342
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2025-04-27 22:12:19 +0530

    Added full update method

--- src/v1/internals/courses-internals.ts
+++ src/v1/internals/courses-internals.ts
@@ -54,6 +54,38 @@ export async function get_course_by_id(req: Request, res: Response) {
     }
 }
 
+export async function get_course_by_name(req: Request, res: Response) {
+    try {
+        const { db, client } = await get_connection("admission-automation")
+
+        const name = req.params["course_name"]
+
+        const result = await db.collection("courses").find({ name }).toArray()
+
+        if (result.length === 0) {
+            res.status(404).json({
+                data: {},
+                message: "Course with this name does not exist"
+            })
+            return
+        }
+
+        res.status(200).json({
+            data: result as (WithId<Document> & CourseWithID)[],
+            message: "Data fetch Success"
+        })
+
+        client.close()
+        return
+    } catch (e) {
+        res.status(400).json({
+            data: {},
+            message: "" + e
+        })
+        return
+    }
+}
+
 export async function add_new_course(req: Request, res: Response) {
     try {
         const { db, client } = await get_connection("admission-automation")
@@ -70,6 +102,34 @@ export async function add_new_course(req: Request, res: Response) {
         res.status(400).json({
             data: [],
             message: e
+        })
+        return
+    }
+}
+
+export async function update_course_by_id(req: Request, res: Response) {
+    try {
+        const { db, client } = await get_connection("admission-automation")
+        const id = new ObjectId(req.params["id"])
+
+        const updated_res = await db.collection("courses").replaceOne({ _id: id }, req.body)
+        if (updated_res.matchedCount === 0 || updated_res.modifiedCount === 0) {
+            res.status(404).json({
+                data: {},
+                message: "No such entry found"
+            })
+            return
+        }
+
+        client.close()
+        res.status(200).json({
+            data: { id, ...req.body },
+            message: "Data update success"
+        })
+    } catch (e) {
+        res.status(400).json({
+            data: {},
+            message: "" + e
         })
         return
     }
