bf4212fb5e7df5d3eb0962d58868d34988fa9829
Sanjeev Singh <sanjeev.2311.singh@gmail.com>
2025-05-11 18:20:54 +0530

    added bare minimum logging

--- src/app.ts
+++ src/app.ts
@@ -1,10 +1,12 @@
 import express, { json } from "express"
 import setup_v1_router from "./v1/app-router"
+import logger from "./middlewares/logger"
 
 const app: express.Express = express()
 const PORT = 5729
 
 app.use(json())
+app.use(logger)
 setup_v1_router(app)
 
 app.listen(PORT, "0.0.0.0", () => {
